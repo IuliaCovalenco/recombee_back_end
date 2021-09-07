14f591af18b8520df6da453e2d98797d3531b2bb
IuliaCovalenco <61017754+IuliaCovalenco@users.noreply.github.com>
2021-09-07 23:05:50 +0100

    #commit_cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const multer = require('multer')
 const path = require('path')
 
 dotenv.config()
-app.use(cors())
+app.use(cors({origin: '*'}))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
